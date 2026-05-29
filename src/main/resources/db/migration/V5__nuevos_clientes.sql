cbf9087ca5a3785805c996e86130eb8190de04b8
AgustinCoup <tutyboss12@gmail.com>
2026-05-29 01:09:30 -0300

    Añado nuevos clientes para el release de mañana. Hay que corregir la tarea programada che

--- src/main/resources/db/migration/V5__nuevos_clientes.sql
+++ src/main/resources/db/migration/V5__nuevos_clientes.sql
@@ -1,5 +1,5 @@
 -- Clientes a eliminar:
-DELETE FROM clientes WHERE nombre IN (
+DELETE IGNORE FROM clientes WHERE nombre IN (
     'MINISTERIO DE SALUD'
 );
 
@@ -11,4 +11,19 @@ INSERT IGNORE INTO clientes (nombre) VALUES
 ('Ministerio de Salud - Marcos Juárez'),
 ('Ministerio de Salud - Corral de Bustos'),
 ('Ministerio de Salud - Huinca Renancó'),
-('Ministerio de Salud - Laboulaye');
+('Ministerio de Salud - Laboulaye'),
+('Abdala, Victor Esteban'),
+('Vettorazzi, María Luz - RYCEA'),
+('Santa Juliana, Luis'),
+('Palacios, Santiado - Clinica del niño'),
+('Maffrand'),
+('Serra, María'),
+('Etcheverry, Sofía'),
+('Ballesté, Luciana'),
+('IMEB'),
+('Kremer, Mauricio'),
+('Aznar, Ivan - Civus'),
+('Oyola, Ricardo'),
+('Conti, Adriana'),
+('Imperio Dental'),
+('Roasio, Maximiliano');
